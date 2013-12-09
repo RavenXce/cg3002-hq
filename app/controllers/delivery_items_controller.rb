b63ae4a9b3bf1263b51b260285038fe02296ad7a
Ivan Poon <ravenxce@gmail.com>
2013-12-09 14:54:41 +0800

    Added error checking

--- app/controllers/delivery_items_controller.rb
+++ app/controllers/delivery_items_controller.rb
@@ -2,6 +2,13 @@ class DeliveryItemsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:edit]
   
   def edit # jqGrid is not RESTful =[
+    @delivery = ShopDelivery.find(params[:delivery_id])
+    if @delivery.nil? then
+      render json: {:errors => "Invalid delivery ID"}, status: 422 and return
+    end
+    if @delivery.status != "approved" && @delivery.status != "pending" then
+      render json: {:errors => "Shipment has already left."}, status: 422 and return
+    end    
     operation = params[:oper]
     if operation == 'edit'
       update
@@ -15,12 +22,11 @@ class DeliveryItemsController < ApplicationController
   private
   
   def create
-    delivery = ShopDelivery.find(params[:delivery_id])
-    item = Item.find_by_barcode(params["items.barcode"])
-    if item.nil? || delivery.nil?
-      render json: {:message => "Invalid item barcode."}, status: 422 and return
+    item = ShopItem.includes(:item).where("items.barcode = ?", params["items.barcode"]).first
+    if item.nil? then
+      render json: {:errors => "Invalid item barcode."}, status: 422 and return
     end
-    delivery_item = delivery.shop_delivery_items.find_or_create_by_item_id(item.id)
+    delivery_item = @delivery.shop_delivery_items.find_or_create_by_item_id(item.item_id)
     delivery_item.quantity ||= 0 
     delivery_item.quantity += params[:quantity].to_i
     delivery_item.save
